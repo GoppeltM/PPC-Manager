a3540b4c8b4fd3c1fee4bfc0b0205a265a694d6b
Marius Goppelt <MGoppelt@gmx.de>
2010-09-20 23:10:08 +0200

    "Vereine bearbeiten.xaml" umbenannt in "Optionen.xaml", weil dort noch mehr Funktionen hinein gehören.
    Ganz allgemein komplexeres Layout, und Binden an Settings.

--- Trunk/PPC Manager/PPC Manager/MainWindow.xaml.vb
+++ Trunk/PPC Manager/PPC Manager/MainWindow.xaml.vb
@@ -2,17 +2,6 @@
 
 Class MainWindow
 
-    Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs) Handles Me.Loaded
-
-    End Sub
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
-        Dim keys = Me.Resources.Item("myPersonen")
-        Console.WriteLine()
-        
-
-    End Sub
-
     Private Sub MenuItem1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Öffnen.Click
         Dim dialog = New OpenFileDialog
         With dialog
@@ -24,15 +13,7 @@ Class MainWindow
     End Sub
 
     Private Sub Vereine_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Vereine.Click
-        Dim dialog As New Vereine_bearbeiten
-        Dim result = dialog.ShowDialog
-        If result = True Then
-            My.Settings.Vereine.Clear()
-            For Each verein In dialog.ListBox1.Items
-                My.Settings.Vereine.Add(verein)
-            Next
-            My.Settings.Save()
-        End If
-
+        Dim dialog As New Optionen
+        dialog.ShowDialog()
     End Sub
 End Class
---
+++ Trunk/PPC Manager/PPC Manager/Optionen.xaml.vb
@@ -0,0 +1,27 @@
+﻿Public Class Optionen
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button2.Click
+        Me.DialogResult = True
+        My.Settings.Vereine.Clear()
+        For Each verein As Verein In Resources.Item("Vereine")
+            My.Settings.Vereine.Add(verein.Vereinsname)
+        Next
+        My.Settings.Save()
+        Me.Close()
+    End Sub
+End Class
+
+Friend Class VereinsDaten
+    Inherits ObjectModel.ObservableCollection(Of Verein)
+
+    Public Sub New()
+        For Each verein As String In My.Settings.Vereine
+            Me.Add(New Verein With {.Vereinsname = verein})
+        Next
+    End Sub
+End Class
+
+Friend Class Verein
+    Public Property Vereinsname As String
+End Class
