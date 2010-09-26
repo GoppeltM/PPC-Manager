2d575e92c65b0845607d6a515e0124a98113262d
Marius Goppelt <MGoppelt@gmx.de>
2010-09-26 13:03:02 +0200

    Cut/Copy/Paste sauber implementiert. Gott, was für eine Frickelarbeit!
    Merke: Copy lieber selber implementieren, erst Platz schaffen dann reinpasten und Selection manipulieren, Column.GetCellContent holt das tatsächliche Cell Control.
    
    Bekannte Bugs: Strichpunkt und Komma führen sowohl beim Copy als auch beim Paste zu falsch zerlegten Zeilen. Wollte man das umgehen, müsste ich beides auch nochmal für XML in der Zwischenablage implementieren, aber darauf habe ich grad keine Lust
    
    Signed-off-by: Marius Goppelt <MGoppelt@gmx.de>

--- Trunk/PPC Manager/PPC Manager/MainWindow.xaml.vb
+++ Trunk/PPC Manager/PPC Manager/MainWindow.xaml.vb
@@ -17,7 +17,15 @@ Class MainWindow
         dialog.ShowDialog()
     End Sub
 
-    Private Sub MenuItem1_Click_1(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles TurnierStarten.Click
+    Private Sub TurnierStarten_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles TurnierStarten.Click
         Frame1.Navigate(New Uri("pack://application:,,,/Begegnungen.xaml"))
+    End Sub
+
+    Private Sub Close_CanExecute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
+        e.CanExecute = True
+    End Sub
+
+    Private Sub Close_Executed(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)
+        My.Application.MainWindow.Close()
     End Sub
 End Class
--- Trunk/PPC Manager/PPC Manager/StartListe.xaml.vb
+++ Trunk/PPC Manager/PPC Manager/StartListe.xaml.vb
@@ -8,45 +8,114 @@
 
     End Sub
 
-    Private Sub CommandBinding_CanExecute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
-        e.CanExecute = Clipboard.ContainsData("CSV")
+    Private Sub Paste_CanExecute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
+        Dim HasData = Clipboard.ContainsData("CSV")
+        e.CanExecute = HasData AndAlso DataGrid1.SelectedCells.Any
     End Sub
 
-    Private Sub CommandBinding_Executed(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)
+    Private Sub Paste_Executed(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)
         Dim data As String = Clipboard.GetDataObject.GetData("CSV")
-        Dim Items = New List(Of String)
+        Dim ClipboardRows = New List(Of List(Of String))
         Dim Rows = Regex.Split(data, String.Format(Environment.NewLine)).ToList
         Rows.RemoveAt(Rows.Count - 1)
 
         For Each row In Rows
-            Items.AddRange(row.Split(","c, ";"c))
+            ClipboardRows.Add(row.Split(","c, ";"c).ToList)
         Next
-        Dim current = 0
-        For Each cell In DataGrid1.SelectedCells
-            If current >= Items.Count Then current = 0
-            Dim spieler As Spieler
-            If Not TypeOf cell.Item Is Spieler Then
-                spieler = New Spieler
-                Dim liste As SpielerListe = Resources.Item("myPersonen")
-                liste.Add(spieler)
-            Else
-                spieler = cell.Item
-            End If
 
-            Select Case cell.Column.Header
-                Case "Vorname" : spieler.Vorname = Items(current)
-                Case "Nachname" : spieler.Nachname = Items(current)
-                Case "Verein" : spieler.Verein = Items(current)
-                Case "Spielklasse" : spieler.SpielKlasse = Items(current)
-                Case "Position" : Integer.TryParse(Items(current), spieler.Position)
-                Case "Turnierklasse" : spieler.TurnierKlasse = Items(current)
-            End Select
-            current += 1
+
+        Dim liste As SpielerListe = Me.FindResource("myPersonen")
+
+        Dim selectedRows = From x In DataGrid1.SelectedCells Group By x.Item Into Group
+
+        Dim currentRow = 0
+
+        'Wenn letzte Zeile gleich Platzhalter, erstmal Platz machen
+
+        Dim LastSelectedRow = selectedRows.Last
+        If Not LastSelectedRow.Item.GetType Is GetType(Spieler) Then
+            Dim selectedCells = LastSelectedRow.Group
+
+            For i = selectedRows.Count To ClipboardRows.Count
+                Dim spieler = New Spieler
+                liste.Add(spieler)
+                For Each cellInfo In selectedCells
+                    Dim newCell As New DataGridCellInfo(spieler, cellInfo.Column)
+                    DataGrid1.SelectedCells.Add(newCell)
+                Next
+                For Each cellInfo In selectedCells
+                    DataGrid1.SelectedCells.Remove(cellInfo)
+                Next
+            Next
+            DataGrid1.UpdateLayout()
+        End If
+
+        For Each row In ClipboardRows
+
+            If currentRow >= selectedRows.Count Then Exit For
+            Dim selectedGroup = selectedRows(currentRow)
+            Dim selectedCells = selectedGroup.Group
+            Dim SelectedRow = selectedRows(currentRow).Item
+
+            Dim currentSpieler = selectedRows(currentRow).Item
+            currentRow += 1
+
+
+            Dim currentClipCell = 0
+            For Each cell In selectedCells
+                If currentClipCell >= row.Count Then Exit For
+                Dim value = row(currentClipCell)
+                currentClipCell += 1
+
+                Dim myProperty As Object = cell.Column.GetCellContent(currentSpieler)
+
+                Select Case myProperty.GetType
+                    Case GetType(TextBlock)
+                        Dim text = CType(myProperty, TextBlock)
+                        text.Text = value
+                    Case GetType(ComboBox)
+                        Dim combo = CType(myProperty, ComboBox)
+                        combo.SelectedItem = value
+                End Select
+            Next
         Next
+
     End Sub
 
-    Private Sub DataGrid1_Drop(ByVal sender As System.Object, ByVal e As System.Windows.DragEventArgs) Handles DataGrid1.Drop
-        Dim data As String = e.Data.GetData("CSV")
+    Private Sub Cut_CanExecute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
+        e.CanExecute = DataGrid1.SelectedItems.Count > 0
+    End Sub
 
+    Private Sub Cut_Executed(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)        
+        ApplicationCommands.Copy.Execute(DataGrid1.SelectedCells, DataGrid1)
+        ApplicationCommands.Delete.Execute(DataGrid1.SelectedCells, DataGrid1)
+    End Sub
+
+    Private Sub Copy_CanExecute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
+        e.CanExecute = DataGrid1.SelectedCells.Any
+    End Sub
+
+    Private Sub CommandBinding_Executed(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)
+        Dim selectedRows = From x In DataGrid1.SelectedCells Group By x.Item Into Group
+
+
+        Dim CSVContent = String.Empty
+        For Each row In selectedRows
+            Dim CellContents As New List(Of String)
+            For Each cell In row.Group
+                Dim myProperty As Object = cell.Column.GetCellContent(row.Item)
+                Select Case myProperty.GetType
+                    Case GetType(TextBlock)
+                        Dim text = CType(myProperty, TextBlock)
+                        CellContents.Add(text.Text)
+                    Case GetType(ComboBox)
+                        Dim combo = CType(myProperty, ComboBox)
+                        CellContents.Add(combo.SelectedItem)
+                End Select
+            Next
+            CSVContent &= String.Join(";"c, CellContents.ToArray)
+            CSVContent &= Environment.NewLine
+        Next
+        Clipboard.SetData(DataFormats.CommaSeparatedValue, CSVContent)
     End Sub
 End Class
