a96cc5017ba68cdd44189f2dacc53e84e75942cd
Marius Goppelt <MGoppelt@gmx.de>
2013-02-15 21:09:32 +0100

    UI aufgebohrt, so dass Freilose sowohl im Drucken als auch im Hauptfenster ihr eigenes Layout bekommen können
    Signed-off-by: Marius Goppelt <MGoppelt@gmx.de>

--- Trunk/PPC Manager/PPC Manager/DesignData.vb
+++ Trunk/PPC Manager/PPC Manager/DesignData.vb
@@ -9,7 +9,7 @@
         For Each partie In New DesignSpielPartien
             SpielRunde.Add(partie)
         Next        
-        SpielRunde.AusgeschiedeneSpieler.Add(spielerListe.Last)
+        SpielRunde.AusgeschiedeneSpieler.Add(spielerListe.Last)        
         Me.Push(SpielRunde)
     End Sub
 
@@ -29,6 +29,7 @@ Public Class DesignSpielPartien
             partie.Add(New Satz With {.PunkteLinks = 0, .PunkteRechts = 11})
             Me.Add(partie)
         Next
+        Me.Add(New FreiLosSpiel(New Spieler With {.Vorname = "Günther", .Nachname = "Netzer"}))
     End Sub
 
     Public Shared ReadOnly Property DesignSpielPartie As SpielPartie
@@ -55,5 +56,23 @@ Friend Class DesignSpielerListe
         Add(New Spieler() With {.Vorname = "Catharina", .Nachname = "Sforza", .TTRating = 8})
         Add(New Spieler() With {.Vorname = "Adam", .Nachname = "Jensen", .TTRating = 9})
         Add(New Spieler() With {.Vorname = "Miroslav", .Nachname = "Klose", .TTRating = 10})
+        Add(New Spieler() With {.Vorname = "Che", .Nachname = "Guevara", .TTRating = 11})
+        Add(New Spieler() With {.Vorname = "Friedrich", .Nachname = "Nietzsche", .TTRating = 12})
+        Add(New Spieler() With {.Vorname = "Johann Wolfgang von", .Nachname = "Goethe", .TTRating = 13})
+        Add(New Spieler() With {.Vorname = "Margaret", .Nachname = "Thatcher", .TTRating = 14})
+        Add(New Spieler() With {.Vorname = "Marie", .Nachname = "Curie", .TTRating = 15})
+        Add(New Spieler() With {.Vorname = "Albert", .Nachname = "Einstein", .TTRating = 16})
+        Add(New Spieler() With {.Vorname = "Julius", .Nachname = "Cäsar", .TTRating = 17})
+        Add(New Spieler() With {.Vorname = "Lucius", .Nachname = "Verenus", .TTRating = 18})
+        Add(New Spieler() With {.Vorname = "Titus", .Nachname = "Pullo", .TTRating = 19})
+        Add(New Spieler() With {.Vorname = "Mick", .Nachname = "Jagger", .TTRating = 20})
+        Add(New Spieler() With {.Vorname = "Paul", .Nachname = "Ryan", .TTRating = 21})
+        Add(New Spieler() With {.Vorname = "Barack", .Nachname = "Obama", .TTRating = 22})
+        Add(New Spieler() With {.Vorname = "Francois", .Nachname = "Mitterrand", .TTRating = 23})
+        Add(New Spieler() With {.Vorname = "Napoleon", .Nachname = "Bonaparte", .TTRating = 24})
+        Add(New Spieler() With {.Vorname = "Marty", .Nachname = "McFly", .TTRating = 25})
+        Add(New Spieler() With {.Vorname = "Marilyn", .Nachname = "Monroe", .TTRating = 26})
+        Add(New Spieler() With {.Vorname = "Freddy", .Nachname = "Mercury", .TTRating = 27})
+        Add(New Spieler() With {.Vorname = "Stephen", .Nachname = "Hawking", .TTRating = 28})
     End Sub
 End Class
