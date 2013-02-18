5b35332a8950c5451f57a5e0692afae55e64d7cc
Marius Goppelt <MGoppelt@gmx.de>
2013-02-18 21:28:00 +0100

    Neues Projekt hinzugefügt, was die Grundlage bilden soll um einerseits die eintreffenden Spieler am Turnierbeginn zu bestätigen, und andererseits Fremdspieler in die XML zu injizieren.
    Signed-off-by: Marius Goppelt <MGoppelt@gmx.de>

---
+++ Trunk/PPC Manager/StartlistenEditor/MainWindow.xaml.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.Win32
+
+Class MainWindow
+
+    Private Doc As XDocument
+
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+
+        With New OpenFileDialog
+            .Filter = "Click-TT Turnierdaten|*.xml"
+            If Not .ShowDialog Then
+                Application.Current.Shutdown()
+                Return
+            End If
+
+            Doc = XDocument.Load(.FileName)
+        End With
+
+        Dim SpielerListe = From competition In Doc.Root.<competition>
+                           From Spieler In competition...<person>
+                           Group Spieler, competition By Spieler.Attribute("internal-nr").Value Into Group
+                           Select Group
+
+        Dim AlleSpieler As New List(Of Spieler)
+
+        For Each s In SpielerListe
+
+            Dim SpielerKnoten = From x In s Select x.Spieler
+
+            Dim Competitions = From x In s Select x.competition.Attribute("age-group").Value
+            AlleSpieler.Add(Spieler.FromXML(SpielerKnoten, Competitions))
+        Next
+
+
+
+    End Sub
+End Class
---
+++ Trunk/PPC Manager/StartlistenEditor/Spieler.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Collections.ObjectModel
+
+Public Class Spieler
+
+    Property Vorname As String
+    Property Nachname As String
+    Property Fremd As Boolean
+    Property XmlKnoten As IEnumerable(Of XElement)
+    Property Anwesend As Boolean
+    Property TTR As Integer
+    Property Klassements As IEnumerable(Of String)
+
+    Shared Function FromXML(SpielerKnoten As IEnumerable(Of XElement), Competitions As IEnumerable(Of String)) As Spieler
+        Return New Spieler With {
+        .XmlKnoten = SpielerKnoten,
+        .Klassements = Competitions
+        }
+    End Function
+
+End Class
+
+Public Class SpielerListe
+    Inherits ObservableCollection(Of Spieler)
+
+    Public Sub New()
+        Add(New Spieler With {.Vorname = "Marius", .Nachname = "Goppelt", .Klassements = {"Herren A", "Herren B"}})
+        Add(New Spieler With {.Vorname = "Flo", .Nachname = "Ewald", .Fremd = True})
+    End Sub
+
+End Class
