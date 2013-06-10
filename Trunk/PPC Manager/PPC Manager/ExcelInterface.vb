5637f546db9eb5506f2c3d056ac32f2f58aed684
Marius Goppelt <MGoppelt@gmx.de>
2013-06-10 19:21:25 +0200

    Umstellung von Excel Interop auf Open XML SDK 2.5 . Nachdem ich selbst auf Windows 8 so meine Mühe hatte die Interop Assemblies zu installieren, habe ich mir doch die Mühe gemacht, das ganze direkt auf Open XML aufzusetzen. Das ist letztendlich die sauberere, nebenwirkungsfreiere, aber auch komplexere Lösung.
    Aktuell bekannte Probleme:
    Aus irgendeinem Grunde erzeugt der Export hin und wieder eine "PPC 15 Anmeldungen Herren DMariusDesktop.xlsx". Kann mir das momentan nicht erklären, vermute aber dass er dort hinspeichert wenn er sonst nirgendwo hin kann.
    
    Signed-off-by: Marius Goppelt <MGoppelt@gmx.de>

--- Trunk/PPC Manager/PPC Manager/ExcelInterface.vb
+++ Trunk/PPC Manager/PPC Manager/ExcelInterface.vb
@@ -1,100 +1,231 @@
-﻿Imports Microsoft.Office.Interop.Excel
-Imports System.Runtime.InteropServices.Marshal
+﻿Imports System.Runtime.InteropServices.Marshal
+Imports System.Text
+Imports DocumentFormat.OpenXml
+Imports DocumentFormat.OpenXml.Packaging
+Imports DocumentFormat.OpenXml.Spreadsheet
+
 
 Public Class ExcelInterface
+    Implements IDisposable
+
+
+    Private Sub New(filePath As String)
+        'If IO.File.Exists(filePath) Then
+        '    doc = OpenExistingExcelDocument(filePath)
+        'Else
+        doc = CreateEmptyExcelDocument(filePath)
+        'End If
+    End Sub
+
+    Private Property doc As SpreadsheetDocument
 
     Public Shared Sub CreateFile(ByVal filePath As String, ByVal spieler As IEnumerable(Of Spieler), ByVal spielrunden As SpielRunden)
-        Dim excel As Microsoft.Office.Interop.Excel.Application = Nothing
-        Try
-            excel = New Microsoft.Office.Interop.Excel.Application
-            excel.Visible = False
-            Dim wb = excel.Workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet)
-            Dim sheet = CType(wb.Worksheets.Add, Worksheet)
-            sheet.Name = "Spieler"
 
-            WriteSpielerSheet(spieler, sheet)
-            WriteRunden(spielrunden, wb)
-           
-            If IO.File.Exists(filePath) Then
-                IO.File.Delete(filePath)
-            End If
-            wb.SaveAs(filePath, XlFileFormat.xlWorkbookDefault)
-            wb.Close()
-        Finally
-            If excel IsNot Nothing Then
-                excel.Quit()
-                ReleaseComObject(excel)
-            End If
 
-        End Try
+        Using ex = New ExcelInterface(filePath)
+            With ex
+                Dim RundeNr = spielrunden.Count.ToString.PadLeft(2, "0"c)
+                Dim sheet = .AddSheet("sp_rd" & RundeNr)                
+                .WriteSpielerSheet(spieler, sheet)
 
+                Dim current = 1
+                For Each runde In spielrunden
+                    Dim currentName = current.ToString.PadLeft(2, "0"c)
+                    sheet = .AddSheet("erg_rd" & currentName)                    
+                    .WriteRunde(spielrunden, sheet)
+                    current += 1
+                Next
+            End With
+        End Using
 
     End Sub
 
-    Private Shared Sub WriteSpielerSheet(ByVal spieler As IEnumerable(Of Spieler), ByVal sheet As Worksheet)
-        Dim Titles = {"Vorname", "Nachname", "Verein", "Spielklasse",
-                          "Position", "Turnierklasse", "Startnummer",
-                          "TTRang", "TTRating", "Punkte", "Buchholzpunkte",
+    Private Sub WriteSpielerSheet(ByVal spieler As IEnumerable(Of Spieler), ByVal sheet As Worksheet)
+        Dim Titles = {"Vorname", "Nachname", "Verein", "ID", "TTRating", "Punkte", "Buchholzpunkte",
                           "Gewonnene Sätze", "Verlorene Sätze", "Gegnerprofil"}
-        Dim column = 1
-        For Each title In Titles
-            CType(sheet.Cells(1, column), Range).Value2 = title
-            column += 1
-        Next
 
-        Dim current = 2
+
+        Dim SheetData = sheet.GetFirstChild(Of SheetData)()
+        
+
+        CreateRow(SheetData, 1, Titles)
+
+        Dim current = 2UI
+
         For Each s In spieler
-            CType(sheet.Cells(current, 1), Range).Value2 = s.Vorname
-            CType(sheet.Cells(current, 2), Range).Value2 = s.Nachname
-            CType(sheet.Cells(current, 3), Range).Value2 = s.Vereinsname            
-            CType(sheet.Cells(current, 7), Range).Value2 = s.Id
-            CType(sheet.Cells(current, 9), Range).Value2 = s.TTRating
-            CType(sheet.Cells(current, 10), Range).Value2 = s.Punkte
-            CType(sheet.Cells(current, 11), Range).Value2 = s.BuchholzPunkte
 
             Dim AlleSätze = Aggregate x In s.GespieltePartien Into Sum(x.Count)
             Dim ss = s
             Dim meineSätze = Aggregate x In s.GespieltePartien Into Sum(x.MeineGewonnenenSätze(ss).Count)
-
-            CType(sheet.Cells(current, 12), Range).Value2 = meineSätze
-            CType(sheet.Cells(current, 13), Range).Value2 = AlleSätze - meineSätze
-
-            column = 14
             Dim gegnerProfil = From x In ss.GespieltePartien Select x.MeinGegner(ss).Id
 
-            For Each gegner In gegnerProfil
-                CType(sheet.Cells(current, column), Range).Value2 = gegner
-                column += 1
-            Next
-            current += 1
-        Next
-    End Sub
-
-    Private Shared Sub WriteRunden(ByVal spielrunden As SpielRunden, ByVal workbook As Workbook)
-
-        Dim currentRunde = 1
-        For Each runde In spielrunden
-            Dim sheet = CType(workbook.Worksheets.Add, Worksheet)
-            sheet.Name = "Runde_" & currentRunde
-            currentRunde += 1
-
-            Dim Titles = {"Linker Spieler", "Rechter Spieler", "Sätze Links", "Sätze Rechts"}
-            Dim currentTitle = 1
-            For Each title In Titles
-                CType(sheet.Cells(1, currentTitle), Range).Value2 = title
-                currentTitle += 1
-            Next
-            Dim current = 2
-            For Each ergebnis In runde
-                CType(sheet.Cells(current, 1), Range).Value2 = ergebnis.SpielerLinks.Id
-                CType(sheet.Cells(current, 2), Range).Value2 = ergebnis.SpielerRechts.Id
-                CType(sheet.Cells(current, 3), Range).Value2 = ergebnis.MeineGewonnenenSätze(ergebnis.SpielerLinks).Count
-                CType(sheet.Cells(current, 4), Range).Value2 = ergebnis.MeineGewonnenenSätze(ergebnis.SpielerRechts).Count
-                current += 1
-            Next
+            Dim Werte = {s.Vorname, s.Nachname, s.Vereinsname, s.Id.ToString, s.TTRating.ToString,
+                         s.Punkte.ToString, s.BuchholzPunkte.ToString, meineSätze.ToString,
+                         (AlleSätze - meineSätze).ToString}.Concat(gegnerProfil)
+            CreateRow(SheetData, current, Werte)
+            current += 1UI
         Next
 
     End Sub
 
+    Private Sub WriteRunde(ByVal spielrunden As SpielRunden, ByVal worksheet As Worksheet)
+        Dim SheetData = worksheet.GetFirstChild(Of SheetData)()
+        Dim Titles = {"Linker Spieler", "Rechter Spieler", "Sätze Links", "Sätze Rechts"}
+        CreateRow(SheetData, 1, Titles)
+
+        Dim current = 2UI
+
+        For Each ergebnis In spielrunden.Peek
+            Dim Werte = {ergebnis.SpielerLinks.Id, ergebnis.SpielerRechts.Id, ergebnis.MeineGewonnenenSätze(ergebnis.SpielerLinks).Count.ToString,
+                         ergebnis.MeineGewonnenenSätze(ergebnis.SpielerRechts).Count.ToString}
+            CreateRow(SheetData, current, Werte)
+            current += 1UI
+        Next
+
+    End Sub
+
+    Private Sub CreateRow(sheetdata As SheetData, rowIndex As UInteger, entries As IEnumerable(Of String))
+        Dim row = New Row
+        row.RowIndex = rowIndex
+        sheetdata.Append(row)
+        Dim AValue = Convert.ToInt32("A"c)
+        Dim ValueCells = entries.Zip(Enumerable.Range(AValue, Integer.MaxValue - AValue), Function(x, y) New With {x, .y = Convert.ToChar(y)})
+        For Each Value In ValueCells
+            Dim CachedCellValue = InsertSharedStringItem(Value.x, doc.WorkbookPart.SharedStringTablePart)
+            Dim CellName = Value.y & row.RowIndex.Value
+            row.AppendChild(Of Cell)(New Cell With {.CellValue = New CellValue(CachedCellValue.ToString),
+                            .CellReference = CellName, .DataType = New EnumValue(Of CellValues)(CellValues.SharedString)})
+        Next
+    End Sub
+
+
+
+    Private Function OpenExistingExcelDocument(path As String) As SpreadsheetDocument
+        Dim MissCount = 0
+        Dim spreadsheetDocument As SpreadsheetDocument = Nothing
+        While spreadsheetDocument Is Nothing
+            Try
+                spreadsheetDocument = spreadsheetDocument.Open(path, True)
+                Exit While
+            Catch ex As Exception When Not MissCount >= 20
+                MissCount += 1
+                System.Threading.Thread.Sleep(200)
+            End Try
+        End While
+        Return spreadsheetDocument
+    End Function
+
+    Private Function CreateEmptyExcelDocument(path As String) As SpreadsheetDocument
+        ' Create a spreadsheet document by supplying the filepath.
+        ' By default, AutoSave = true, Editable = true, and Type = xlsx.
+        Dim spreadsheetDocument As SpreadsheetDocument = Nothing
+        Dim MissCount = 0
+        While spreadsheetDocument Is Nothing
+            Try
+                spreadsheetDocument = spreadsheetDocument.Create(path, SpreadsheetDocumentType.Workbook, True)
+                Exit While
+            Catch ex As Exception When Not MissCount >= 20
+                MissCount += 1
+                System.Threading.Thread.Sleep(200)
+            End Try
+        End While
+
+
+        ' Add a WorkbookPart to the document.
+        Dim workbookpart As WorkbookPart = spreadsheetDocument.AddWorkbookPart
+        workbookpart.Workbook = New Workbook
+
+        Dim stringTablePart = workbookpart.AddNewPart(Of SharedStringTablePart)()
+        stringTablePart.SharedStringTable = New SharedStringTable
+
+        ' Add Sheets to the Workbook.
+        Dim sheets As Sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(Of Sheets)(New Sheets())
+        Return spreadsheetDocument
+    End Function
+
+    Private Function AddSheet(sheetName As String) As Worksheet
+        ' Add a WorksheetPart to the WorkbookPart.
+        Dim worksheetPart As WorksheetPart = doc.WorkbookPart.AddNewPart(Of WorksheetPart)()
+        worksheetPart.Worksheet = New Worksheet(New SheetData())
+
+        Dim Sheets = doc.WorkbookPart.Workbook.Sheets
+        Dim SheetIDs = (From x In Sheets.Elements(Of Sheet)() Select x.SheetId.Value).ToList
+
+        Dim CurrentId = 1UI
+        While SheetIDs.Contains(CurrentId)
+            CurrentId += 1UI
+        End While
+
+        ' Append a new worksheet and associate it with the workbook.
+        Dim sheet As Sheet = New Sheet
+        sheet.Id = doc.WorkbookPart.GetIdOfPart(worksheetPart)
+        sheet.SheetId = CurrentId
+        sheet.Name = sheetName
+
+        Sheets.Append(sheet)
+        Return worksheetPart.Worksheet
+    End Function
+
+    Private Function GetWorkSheet(sheetName As String) As Worksheet        
+        For Each Sheet In doc.WorkbookPart.Workbook.Sheets.Elements(Of Sheet)()            
+            If Sheet.Name = sheetName Then
+                Dim ID = doc.WorkbookPart.GetPartById(Sheet.Id)
+                Return DirectCast(ID, WorksheetPart).Worksheet
+            End If
+        Next
+        Throw New ArgumentException("No sheet found!", sheetName)
+    End Function
+
+    ' Given text and a SharedStringTablePart, creates a SharedStringItem with the specified text 
+    ' and inserts it into the SharedStringTablePart. If the item already exists, returns its index.
+    Private Function InsertSharedStringItem(ByVal text As String, ByVal shareStringPart As SharedStringTablePart) As Integer
+        Dim i As Integer = 0
+
+        ' Iterate through all the items in the SharedStringTable. If the text already exists, return its index.
+        For Each item As SharedStringItem In shareStringPart.SharedStringTable.Elements(Of SharedStringItem)()
+            If (item.InnerText = text) Then
+                Return i
+            End If
+            i = (i + 1)
+        Next
+
+        ' The text does not exist in the part. Create the SharedStringItem and return its index.
+        shareStringPart.SharedStringTable.AppendChild(New SharedStringItem(New DocumentFormat.OpenXml.Spreadsheet.Text(text)))
+        shareStringPart.SharedStringTable.Save()
+
+        Return i
+    End Function
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                doc.Dispose()
+                ' TODO: dispose managed state (managed objects).
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        Me.disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(ByVal disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
 
 End Class
