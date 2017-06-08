6172c73cfe8b4e6423ef822d02443bc6f94c8a03
Marius Goppelt <mgoppelt@outlook.com>
2017-06-08 13:24:43 +0200

    Fixes #15 Rundenname der selektierten Partie wird in der Detailsicht mit visualisiert. Damit ist die aktuelle Runde immer sichtbar - selbst für Playoffs

--- Trunk/PPC Manager/PPC Manager Tests/View/SpielPartieDetailTests.vb
+++ Trunk/PPC Manager/PPC Manager Tests/View/SpielPartieDetailTests.vb
@@ -6,7 +6,15 @@ Public Class SpielPartieDetailTests
     <Test>
     Public Sub UIDummy()
         Dim x = New SpielPartieDetail()
-        x.DataContext = New SpielPartie("Runde 1", New SpielerInfo("1"), New SpielerInfo("2"))
+        x.DataContext = New SpielPartie("Runde 1",
+                                        New SpielerInfo("1") With {
+         .Vorname = "Marius",
+         .Nachname = "Goppelt"
+        },
+                                        New SpielerInfo("2") With {
+         .Vorname = "Max",
+         .Nachname = "Mustermann"
+        })
         Dim w = New Window
         w.Content = x
         w.ShowDialog()
