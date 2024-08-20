db2eda6c96048f219204a2736356a5c91f77ec49
Nikolaj Kappler <info@nkappler.de>
2024-08-20 20:56:22 +0200

    manueller Urkunden Dialog

--- Trunk/PPC Manager/PPC Manager/View/Hauptmenü.xaml.vb
+++ Trunk/PPC Manager/PPC Manager/View/Hauptmenü.xaml.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Hauptmenü
+    Private Sub MenuItem_Click(sender As Object, e As RoutedEventArgs)
 
+        Dim dialog = New UrkundeManuellDialog()
+
+        dialog.ShowDialog()
+    End Sub
 End Class
---
+++ Trunk/PPC Manager/PPC Manager/View/UrkundeManuellDialog.xaml.vb
@@ -0,0 +1,41 @@
+﻿
+
+Public Class UrkundeManuellDialog
+
+    Public Sub New()
+        InitializeComponent()
+
+        Vorschau.Datum1.Text = Date.Today.ToShortDateString()
+        Vorschau.Datum2.Text = Date.Today.ToLongDateString()
+    End Sub
+
+    Private Sub txtName_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtName.TextChanged
+        Vorschau.SpielerName.Text = txtName.Text
+    End Sub
+
+    Private Sub txtVereinsname_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtVereinsname.TextChanged
+        Vorschau.Vereinsname.Text = txtVereinsname.Text
+    End Sub
+
+    Private Sub txtPlatz_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtPlatz.TextChanged
+        Vorschau.Platzierung.Text = "zum " & txtPlatz.Text & ". Platz"
+    End Sub
+
+    Private Sub txtWettbewerbname_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtWettbewerbname.TextChanged
+        Vorschau.WettbName.Text = "im Wettbewerb " & txtWettbewerbname.Text
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim Urkunde = New Urkunde
+        Urkunde.SpielerName.Text = txtName.Text
+        Urkunde.Vereinsname.Text = txtVereinsname.Text
+        Urkunde.Platzierung.Text = "zum " & txtPlatz.Text & ". Platz"
+        Urkunde.WettbName.Text = "im Wettbewerb " & txtWettbewerbname.Text
+
+        Urkunde.Datum1.Text = Date.Today.ToShortDateString()
+        Urkunde.Datum2.Text = Date.Today.ToLongDateString()
+
+        Dim p = New PrintDialog
+        p.PrintVisual(Urkunde, "Urkunde")
+    End Sub
+End Class
