ec6bb70dc96159ebd98cfdc607c0dc18845417cc
Nikolaj Kappler <info@nkappler.de>
2024-08-23 15:15:38 +0200

    fix #59 Freilosdarstellung im Finale

--- Trunk/PPC Manager/PPC Manager/View/PartieKarte.xaml.vb
+++ Trunk/PPC Manager/PPC Manager/View/PartieKarte.xaml.vb
@@ -1,3 +1,30 @@
 ﻿Public Class PartieKarte
 
 End Class
+
+
+Public Class PartieIsFreilosVisibilityConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+        Return If(TypeOf value Is FreiLosSpiel, Visibility.Visible, Visibility.Collapsed)
+
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Throw New NotImplementedException()
+    End Function
+End Class
+
+Public Class PartieVisibilityConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+        Return If(TypeOf value IsNot FreiLosSpiel, Visibility.Visible, Visibility.Collapsed)
+
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Throw New NotImplementedException()
+    End Function
+End Class
